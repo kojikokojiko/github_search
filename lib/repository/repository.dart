2881637ac78549b892d720df7f99c74754862453
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-05-23 16:56:37 +0900

    searchRepositories関数を定義

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -7,5 +7,20 @@ import 'package:github_search/model/repository_entity.dart';
 class GithubRepository{
   GithubRepository(this._apiClient);
   final GithubApiClient _apiClient;
-  
+
+  Future<List<RepositoryEntity>> searchRepositories(String searchKeyword)async{
+    final responseBody=await _apiClient.get('/search/repositories?q=$searchKeyword&sort=stars&order=desc');
+    final decodedJson=json.decode(responseBody) as Map<String,dynamic>;
+    final repositoryList=<RepositoryEntity>[];
+    if(decodedJson["total_count"] as int ==0){
+      return repositoryList;
+    }
+    for(final itemJson in decodedJson["items"]){
+      repositoryList.add(RepositoryEntity.fromJson(itemJson as Map<String ,dynamic>));
+    }
+    return repositoryList;
+    
+
+  }
+
 }
